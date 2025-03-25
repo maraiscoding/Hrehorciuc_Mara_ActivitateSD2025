f5d59fad1649ffd75744b36eaf9fba6543200bf2
maraiscoding <hrehorciucmara22@stud.ase.ro>
2025-03-25 19:03:20 +0200

    Final

--- ListaSimplaCurse.c
+++ ListaSimplaCurse.c
@@ -123,6 +123,56 @@ void dezalocare(Nod** cap) {
 	}
 }
 
+
+char* getCelMaiScumpCal(Nod* lista) {
+	float pretulMaxim = 0;
+	char* detinatorBogat = NULL;
+	while (lista) {
+		if (lista->info.pretLicitatie > pretulMaxim) {
+			pretulMaxim = lista->info.pretLicitatie;
+			detinatorBogat = lista->info.detinator;
+		}
+		lista = lista->next;
+	}
+	if (pretulMaxim > 0) {
+		char* nou = malloc(strlen(detinatorBogat));
+		strcpy_s(nou, strlen(detinatorBogat) + 1, detinatorBogat);
+		return nou;
+	}
+	else {
+		return NULL;
+	}
+}
+
+
+void modificaUrmatoareaCursa(Nod* lista, const char* detinator, const char* urmatoareaCursa, const char* cursaNoua) {
+	while (lista != NULL) {
+		if (strcmp(lista->info.detinator, detinator)==0 && strcmp(lista->info.urmatoareaCursa, urmatoareaCursa) == 0 ) {
+			free(lista->info.urmatoareaCursa);
+			lista->info.urmatoareaCursa = (char*)malloc(sizeof(char) * (strlen(cursaNoua) + 1));
+			strcpy_s(lista->info.urmatoareaCursa, strlen(cursaNoua) + 1, cursaNoua);
+
+		}
+		lista = lista->next;
+	}
+}
+
+float calculeazaPretMediuPerDetinator(Nod* lista, const char* detinator) {
+	if (!lista) {
+		return 0;
+	}
+	float suma = 0;
+	int count = 0;
+	while (lista) {
+		if (strcmp(lista->info.detinator, detinator) == 0) {
+			suma += lista->info.pretLicitatie;
+			count++;
+		}
+		lista = lista->next;
+	}
+	return suma / count;
+}
+
 int main() {
 	Nod* cap = NULL;
 	cap = citesteListaDinFisier("cai.txt");
@@ -130,7 +180,13 @@ int main() {
 
 	printf("Media este:%5.2f\n", calculeazaPretMediu(cap));
 	printf("Caii lui Frederick valoreaza:%5.2f\n", calculeazaPretulCailorUnuiDetinator(cap, "Frederick"));
+	printf("\n%s", getCelMaiScumpCal(cap));
 
-	dezalocare(&cap);
+	
+	modificaUrmatoareaCursa(cap, "Frederick", "Monza", "Nice");
+	printf("Lista dupa modificare:");
+	afisareListaCai(cap);
+
+	printf("Media preturilor cailor lui Hugo este:%5.2f", calculeazaPretMediuPerDetinator(cap, "Hugo"));
 
 }
